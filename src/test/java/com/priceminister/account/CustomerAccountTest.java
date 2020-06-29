a80ce699cb9cc5eb9b3813205806fe94c498497c
Hennadii <erklareb@outlook.com>
2020-06-29 13:28:04 +0200

    refactored code

--- src/test/java/com/priceminister/account/CustomerAccountTest.java
+++ src/test/java/com/priceminister/account/CustomerAccountTest.java
@@ -7,10 +7,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
@@ -20,10 +17,6 @@ import java.util.stream.IntStream;
  * Please create the business code, starting from the unit tests below.
  * Implement the first test, the develop the code that makes it pass.
  * Then focus on the second test, and so on.
- * <p>
- * We want to see how you "think code", and how you organize and structure a simple application.
- * <p>
- * When you are done, please zip the whole project (incl. source-code) and send it to recrutement-dev@priceminister.com
  */
 public class CustomerAccountTest {
 
@@ -44,13 +37,10 @@ public class CustomerAccountTest {
      */
     @Test
     public void testAccountWithoutMoneyHasZeroBalance() throws IllegalBalanceException {
-        // arrange
         Double expectedResult = 0.0d;
 
-        // act
         Double result = customerAccount.getBalance();
 
-        // assert
         Assert.assertEquals(result, expectedResult);
     }
 
@@ -59,15 +49,12 @@ public class CustomerAccountTest {
      */
     @Test
     public void testAddPositiveAmount() throws NegativeAmountException {
-        // arrange
         Double oldBalance = customerAccount.getBalance();
         Double addedAmount = 10.0d;
 
-        // act
         customerAccount.add(addedAmount);
         Double expectedResult = oldBalance + addedAmount;
 
-        // assert
         Assert.assertEquals(expectedResult, customerAccount.getBalance());
     }
 
@@ -76,12 +63,10 @@ public class CustomerAccountTest {
      */
     @Test(expected = NegativeAmountException.class)
     public void addNegativeNumberWillNotChangeBalance() throws NegativeAmountException {
-        // arrange
+
         Double oldBalance = customerAccount.getBalance();
         Double negativeAmount = -10.0d;
-        Double expectedResult = oldBalance;
 
-        // act
         customerAccount.add(negativeAmount);
     }
 
@@ -92,39 +77,34 @@ public class CustomerAccountTest {
     @Test(expected = IllegalBalanceException.class)
     public void testWithdrawAndReportBalanceWillThrowIllegalBalanceIfCurrentBalanceIsZero()
             throws IllegalBalanceException {
-        // arrange
         Double withdrawnAmount = 10.0d;
 
-        // act
         Double result = customerAccount.withdrawAndReportBalance(withdrawnAmount, rule);
     }
 
     @Test(expected = IllegalBalanceException.class)
     public void testWithdrawAndReportBalanceWillThrowIllegalBalanceIfCurrentBalanceLessThenWithdrawnAmount()
             throws IllegalBalanceException, NegativeAmountException {
-        // arrange
+
         Double addedAmount = 9.0d;
         customerAccount.add(addedAmount);
         Double withdrawnAmount = 10.0d;
 
-        // act
         Double result = customerAccount.withdrawAndReportBalance(withdrawnAmount, rule);
     }
 
     @Test
     public void testWithdrawAndReportBalanceWillChangeCurrentBalance()
             throws IllegalBalanceException, NegativeAmountException {
-        // arrange
+
         Double addedAmount = 9.0d;
         customerAccount.add(addedAmount);
         Double withdrawnAmount = 8.0d;
         Double oldBalance = customerAccount.getBalance();
         Double expectedBalance = oldBalance - withdrawnAmount;
 
-        // act
         Double result = customerAccount.withdrawAndReportBalance(withdrawnAmount, rule);
 
-        // assert
         Assert.assertEquals(expectedBalance, result);
     }
 
